d63722cf27e62bc41d74cacb44a3b365d11167e1
hajimeong <ha_01@naver.com>
2025-10-27 21:34:17 +0900

    feat(InputView.java): 자동차들 이름 문자열 입력 및 시도 횟수 입력 기능 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,17 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    //자동차 이름 입력
+    public String inputCarNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    //게임 시도 횟수 입력
+    public Integer inputTryCount(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Integer.valueOf(Console.readLine());
+    }
+}
