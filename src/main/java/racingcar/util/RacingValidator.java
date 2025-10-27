b82ab35c0831ac5d2e696cd3329d57c5a4526261
hajimeong <ha_01@naver.com>
2025-10-27 22:34:52 +0900

    feat(RacingValidator.java): 시도횟수 문자 입력 예외 처리 기능 추가

--- src/main/java/racingcar/util/RacingValidator.java
+++ src/main/java/racingcar/util/RacingValidator.java
@@ -2,9 +2,18 @@ package racingcar.util;
 
 public class RacingValidator {
 
-    public static void validateTryCount(int tryCount) {
-        if(tryCount<=0){
-            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+    public static void validateTryCount(String tryCount) {
+        if(tryCount==null || tryCount.isBlank()){
+            throw new IllegalArgumentException("시도 횟수를 입력해주세요.");
+        }
+
+        try{
+            int count=Integer.parseInt(tryCount);
+            if(count<=0){
+                throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+            }
+        } catch(NumberFormatException e){
+            throw new IllegalArgumentException("시도 횟수는 숫자이어야 합니다.");
         }
     }
 }
